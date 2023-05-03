1b1bf0a1197d9201f45283acea072ba00cffaa35
jananikangesan <janakanges1996@gmail.com>
2023-05-03 15:37:01 +0530

    Customer Management System with Spring Boot

---
+++ src/main/java/com/Computer/cms/model/Customer.java
@@ -0,0 +1,42 @@
+package com.Computer.cms.model;
+
+public class Customer {
+
+    private int customerId;
+    private String customerFirstName;
+    private String customerLastName;
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerFirstName() {
+        return customerFirstName;
+    }
+
+    public void setCustomerFirstName(String customerFirstName) {
+        this.customerFirstName = customerFirstName;
+    }
+
+    public String getCustomerLastName() {
+        return customerLastName;
+    }
+
+    public void setCustomerLastName(String customerLastName) {
+        this.customerLastName = customerLastName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    private String customerEmail;
+}
