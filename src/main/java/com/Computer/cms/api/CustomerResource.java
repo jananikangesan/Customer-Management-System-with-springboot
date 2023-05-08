705dac102fc0f9e0681372e76d4a35064f5cb0e9
jananikangesan <janakanges1996@gmail.com>
2023-05-08 13:49:25 +0530

    GetMapping annotation in Springboot

--- src/main/java/com/Computer/cms/api/CustomerResource.java
+++ src/main/java/com/Computer/cms/api/CustomerResource.java
@@ -1,6 +1,10 @@
 package com.Computer.cms.api;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +24,16 @@ public class CustomerResource {
     public Customer addCustomer(@RequestBody Customer customer){
 
         return customerService.addCustomer(customer);
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerService.getCustomers();
+    }
+
+    @GetMapping(value="/{customerId}")
+    public Customer getCustomer(@PathVariable("customerId") int customerId){
+        return customerService.getCustomer(customerId);
     }
 
 }
