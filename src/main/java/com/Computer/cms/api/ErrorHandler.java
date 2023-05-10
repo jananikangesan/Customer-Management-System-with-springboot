9ce6436d167adcac2485d6a6361353a929068fe0
jananikangesan <janakanges1996@gmail.com>
2023-05-10 14:07:05 +0530

    Customizing an error message

---
+++ src/main/java/com/Computer/cms/api/ErrorHandler.java
@@ -0,0 +1,27 @@
+package com.Computer.cms.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import com.Computer.cms.exception.ApplicationError;
+import com.Computer.cms.exception.CustomerNotFoundException;
+
+@ControllerAdvice
+@RestController
+public class ErrorHandler extends ResponseEntityExceptionHandler{
+    
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<ApplicationError> handleCustomerNotFoundException(CustomerNotFoundException exception , WebRequest webRequest ){
+
+        ApplicationError error = new ApplicationError();
+        error.setCode(101);
+        error.setMessage(exception.getMessage());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+
+    }
+}
