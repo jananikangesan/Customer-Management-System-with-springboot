6f3a53571bb7e6137b7f01698226d398fbd6ea34
jananikangesan <janakanges1996@gmail.com>
2023-05-12 11:20:53 +0530

    WhiteLabel error fixed

--- src/main/java/com/Computer/cms/api/Home.java
+++ src/main/java/com/Computer/cms/api/Home.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Home {
     
-    @GetMapping
+    @GetMapping("/")
     public String home(){
         return "Application is works!!! "+new Date();
     }
